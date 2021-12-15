7a0ba829964b12b81173edcbca2360941b97c685
Your Name <you@example.com>
2021-12-15 08:41:50 -0500

    added more hours

--- assets/script.js
+++ assets/script.js
@@ -36,6 +36,7 @@ var saveTasks = function() {
 $(".saveBtn").on('click', saveTasks);
 
 //display tasks saved in local storage on page
+$('#8 .description').val(localStorage.getItem("8"));
 $('#9 .description').val(localStorage.getItem("9"));
 $('#10 .description').val(localStorage.getItem("10"));
 $('#11 .description').val(localStorage.getItem("11"));
@@ -43,3 +44,5 @@ $('#12 .description').val(localStorage.getItem("12"));
 $('#13 .description').val(localStorage.getItem("13"));
 $('#14 .description').val(localStorage.getItem("14"));
 $('#15 .description').val(localStorage.getItem("15"));
+$('#16 .description').val(localStorage.getItem("16"));
+$('#17 .description').val(localStorage.getItem("17"));
